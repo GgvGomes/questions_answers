00c619ba2294bc4cead130237bba4080d6646e4f
Gabriel Gomes <ggvgabriel05@gmail.com>
2023-10-06 18:05:14 -0300

    atualizações minimas

--- src/components/card_perguntas.tsx
+++ src/components/card_perguntas.tsx
@@ -1,5 +1,10 @@
 export function Card_Perguntas(){
     return (
-        <div className='border-solid border-[2px] border-zinc-600 w-full h-[32%] rounded-xl'></div>
+        <div className='border-solid border-[2px] border-zinc-600 w-full h-[32%] rounded-xl'>
+            <div id="infos">
+                <div id="nome">Nome</div>
+                <div id="data">Data</div>
+            </div>
+        </div>
     )
 }
