1e7b7935defc2024695628b638536e0fc78d3d9c
Gabriel Gomes <ggvgabriel05@gmail.com>
2023-10-11 13:41:57 -0300

    return questions

--- api/routes/showAllQuestions.ts
+++ api/routes/showAllQuestions.ts
@@ -1,9 +1,23 @@
 import { FastifyInstance } from 'fastify';
+import { prisma } from '../lib/prisma';
 
 export async function showAllQuestions(app: FastifyInstance) {
   app.get('/questions', async (request, reply) => {
     const teste = 'Return all QUestions';
 
-    return teste;
+    const prompts = await prisma.questions.findMany({
+      include: {
+        reciver: {
+          select: {
+            name: true,
+          },
+        },
+      },
+      orderBy: {
+        data: 'asc',
+      },
+    });
+
+    return prompts;
   });
 }
