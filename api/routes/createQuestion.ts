001b5cd90166a9eb25814267a4bee3eaef7072ed
Gabriel Gomes <ggvgabriel05@gmail.com>
2023-10-11 13:56:36 -0300

    create question route

--- api/routes/createQuestion.ts
+++ api/routes/createQuestion.ts
@@ -1,9 +1,30 @@
 import { FastifyInstance } from 'fastify';
+import { z } from 'zod';
+import { prisma } from '../lib/prisma';
 
 export async function createQuestion(app: FastifyInstance) {
   app.post('/question', async (request, reply) => {
-    const teste = 'Question';
+    const bodySchema = z.object({
+      question: z.string(),
+      reciverId: z.string().uuid(),
+      transmitter: z.string(),
+    });
 
-    return teste;
+    try {
+      const { question, reciverId, transmitter } = bodySchema.parse(request.body);
+
+      prisma.questions.create({
+        data: {
+          question,
+          reciverId,
+          transmitter,
+        },
+      });
+
+      return { success: 'Question created' };
+    } catch (error) {
+      reply.status(400).send({ error: 'Invalid request body' });
+      return;
+    }
   });
 }
