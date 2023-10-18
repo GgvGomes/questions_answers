bdc2f372860f6df5a2727ac486f40d2950dbf150
GomesZz <ggvgabriel05@gmail.com>
2023-10-17 22:08:02 -0300

    change base url

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: "http://localhost:3333",
+    baseURL: "https://question-aswers-api.vercel.app",
     headers: {
         'Access-Control-Allow-Origin': '*'
     }
